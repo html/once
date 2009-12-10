3e4ceac762a337afe1f6e476c0da0016f75055a2
Olexiy Zamkoviy <olexiy.z@gmail.com>
2009-12-10 22:43:18 +0200

    Initial commit

---
+++ lib/once.rb
@@ -0,0 +1,36 @@
+# Once
+module ActionView
+  module Helpers
+    class Once
+      @@actions = {}
+
+      def self.do(name, &block)
+        if !self.done?(name)
+          self.done(name)
+          return yield
+        end
+        nil
+      end
+
+      def self.done(name)
+        @@actions[name] = true
+      end
+
+      def self.done?(name)
+        @@actions[name]
+      end
+    end
+
+    def once(name, &block)
+      Once::do(name){ yield }
+    end
+
+    def once_done?(name)
+      Once::done?(name)
+    end
+
+    def once_done(name)
+      Once::done(name)
+    end
+  end
+end
---
+++ tasks/once_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :once do
+#   # Task goes here
+# end
